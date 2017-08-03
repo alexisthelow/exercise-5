e04f7b90bc2ee548ada2b620c1f3dd32d467a2fc
Alexis Low <alexisthelow@gmail.com>
2017-08-03 14:53:53 -0600

    second commit, i am all powerful and a terror to behold

--- CreatingVariables.java
+++ CreatingVariables.java
@@ -16,11 +16,19 @@ public class CreatingVariables {
 		lastName = "Mitchell";
 		title = "Mr.";
 		
+		int myAge = 10000000;
+		double powerLevel = 9001;
+		String name = "Alexis";
+		String myTitle = "Queen of All Cosmos";
+		
+		
 		System.out.println("The variable x contains " + x);
 		System.out.println("The value " + y + " is stored in the variable y.");
 		System.out.println("The experiment took " + seconds + " seconds.");
 		System.out.println("A favorite irrational # is Euler's number: " + e);
 		System.out.println("Hopefully you have more than $" + checking);
 		System.out.println("My name's " + title + " " + firstName + " " + lastName);
+		
+		System.out.println("I am " + name + ", " + myTitle + ". I am " + myAge + " years old, and my power level is " + powerLevel + ".");
 	}
 }
